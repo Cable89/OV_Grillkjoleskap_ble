9c4e4900d4ca3656263f1d93a2a06f66ee0b5b3f
medlem <medlem@insult.ed.ntnu.no>
2018-07-16 22:47:33 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdint.h>
+#include <string.h>
+#include "nordic_common.h"
+#include "nrf.h"
+#include "ble_hci.h"
+#include "ble_advdata.h"
+#include "ble_advertising.h"
+#include "ble_conn_params.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_soc.h"
+#include "nrf_sdh_ble.h"
+#include "nrf_ble_gatt.h"
+#include "nrf_ble_qwr.h"
+#include "app_timer.h"
+#include "ble_nus.h"
+#include "app_uart.h"
+#include "app_util_platform.h"
+#include "bsp_btn_ble.h"
+#include "nrf_pwr_mgmt.h"
+
+#if defined (UART_PRESENT)
+#include "nrf_uart.h"
+#endif
+#if defined (UARTE_PRESENT)
+#include "nrf_uarte.h"
+#endif
+
+#include "ble_uart_stuff.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+/**@brief Application main function.
+ */
+int main(void)
+{
+    bool erase_bonds;
+
+    // Initialize.
+    uart_init();
+    log_init();
+    timers_init();
+    buttons_leds_init(&erase_bonds);
+    power_management_init();
+    ble_stack_init();
+    gap_params_init();
+    gatt_init();
+    services_init();
+    advertising_init();
+    conn_params_init();
+
+    // Start execution.
+    printf("\r\nUART started.\r\n");
+    NRF_LOG_INFO("Debug logging for UART over RTT started.");
+    advertising_start();
+
+    // Enter main loop.
+    for (;;)
+    {
+        idle_state_handle();
+    }
+}
---
+++ pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Debug/Obj/ses_nrf52_Vectors.d
@@ -0,0 +1,2 @@
+Output/ble_app_uart_pca10040_s132\ Debug/Obj/ses_nrf52_Vectors.o: \
+ C:\nRF5_SDK_15.0.0\modules\nrfx\mdk\ses_nrf52_Vectors.s
